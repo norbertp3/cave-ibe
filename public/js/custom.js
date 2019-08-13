24d21a3553a6026b99fc110f67642a2c74aa539c
Norbert <norbert@p3.ie>
2019-08-13 11:49:55 +0100

    done home page

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,6 +7,26 @@ $('.wrapper').find('a[href="#"]').on('click', function (e) {
     $(this).closest('.wrapper').find('.small, .big').toggleClass('small big');
 });
 
+// Rotation
 $(".rotate").click(function(){
     $(this).toggleClass("down"); 
+});
+
+// Home 3 boxes same height
+$(".home-3-boxes").each(function(){ 
+    var largest = 0;
+    $(this).find(".home-box-content-text").each(function(){ 
+       var findHeight = $(this).height();
+       if(findHeight > largest){
+          largest = findHeight;
+       }  
+    });
+    $(this).find(".home-box-content-text").css({"height":largest+"px"});
+});
+
+$(".location-nav-dropdown").hover(function(){
+    $(".location-nav").toggleClass("add-nav-hover");
+});
+$(".hotels-nav-dropdown").hover(function(){
+    $(".hotels-nav").toggleClass("add-nav-hover");
 });
