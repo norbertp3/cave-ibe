51da3d6443c781a57ef54a2e1278534151c7a142
Norbert <norbert@p3.ie>
2020-01-24 12:47:47 +0000

    wip

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,19 @@
+// Hamburger
 $(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(this).toggleClass("is-active");
-    });
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
   });
+});
+
+// Mobile detach
+if ($(window).width() < 991) {
+  $("#booking-summary").detach().insertAfter("#mobile-hotel-info");
+  $('.open-booking-summery').click(function(){
+    $('.booking-summary-data').slideToggle();
+    $(this).find('.fa').toggleClass('rotate180');
+});
+}
+// .Fa rotate
+$('.trigger-rotate').click(function(){
+  $(this).find('.fa').toggleClass('rotate180');
+});
