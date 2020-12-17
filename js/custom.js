a5fb73aaa874cf5eaea4efb03f8dee7778b69c58
Adrian <adrian.ziolkowski@gmail.com>
2020-12-17 17:09:42 +0000

    styling & layout update

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
 // Hamburger
-$(document).ready(function(){
-  $('.hamburger').click(function(e) {
+$(document).ready(function () {
+  $('.hamburger').click(function (e) {
     e.preventDefault();
     $('.mobile-nav__collapse').slideToggle();
   });
-  $('.mobile-nav__link--dropdown').click(function(e) {
+  $('.mobile-nav__link--dropdown').click(function (e) {
     e.preventDefault();
     $(this).next('.mobile-nav__sub-nav').slideToggle();
   });
+
 });
 
 // Disable scroll
@@ -15,3 +16,56 @@ function noScroll() {
   window.scrollTo(0, 0);
 }
 
+//show/hide unavailable rooms
+$('.trigger-availability').on('click', function () {
+  $('.unavailable-rooms').slideToggle(1000);
+
+});
+
+$(function () {
+  $("#datepicker1").datepicker();
+  $("#datepicker2").datepicker();
+});
+
+//navigation
+function openNav() {
+  document.getElementById("mySidenav").style.width = "550px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+/* dropdown menu */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+$('.desktop-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false,
+  autoplay: true,
+});
+
+$('.unavailable-rooms-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false,
+  autoplay: true,
+});
--- js/p3core.js
+++ js/p3core.js
@@ -1,48 +1,48 @@
 // Booking Summary Mobile and iPad
 if ($(window).width() < 1200) {
   $("#booking-summary").detach().insertAfter("#mobile-hotel-info");
-  $('.booking-summary-heading').click(function(){
+  $('.booking-summary-heading').click(function () {
     $('.booking-summary-data').slideToggle();
     $(this).find('.icon-expand').toggleClass("rotate180");
   });
 }
 // Mobile detach P3B
 if ($(window).width() < 991) {
-  $('.rate-name').click(function(){
+  $('.rate-name').click(function () {
     $(this).closest('.rate-container').find('.rate-description').detach().insertAfter($(this).closest('.rate-container').find('.rate-name'));
   });
   $(".room-modal-slider").detach().insertAfter(".room-modal-header");
 }
 
 // Booking Summary Room Details
-$('.booking-summary-room-row').click(function(){
+$('.booking-summary-room-row').click(function () {
   $(this).closest('.booking-summary-room-row').find('.booking-summary-room-row-expanded').slideToggle();
 });
 
 // .Fa rotate P3B
-$('.trigger-rotate').click(function(){
+$('.trigger-rotate').click(function () {
   $(this).find('.fa').toggleClass('rotate180');
   $(this).find('i').toggleClass('rotate180');
 });
 
 
 // View Rates P3B
-$('.view-rates-btn').click(function(){
+$('.view-rates-btn').click(function () {
   $(this).closest('.room-with-rates-wrapper').find('.rates-wrapper').slideToggle();
-  $("html, body").animate({
-      scrollTop: $(this).closest('.room-with-rates-wrapper').find('.room-wrapper').offset().top - 10
-  }, 'slow');
+  // $("html, body").animate({
+  //   scrollTop: $(this).closest('.room-with-rates-wrapper').find('.room-wrapper').offset().top - 10
+  // }, 'slow');
 });
-$('.rate-name').click(function(){
+$('.rate-name').click(function () {
   $(this).closest('.rate-container').find('.rate-description').slideToggle();
 });
-$('.expand-special-request').click(function(){
+$('.expand-special-request').click(function () {
   $(this).closest('section').find('.special-request-input').slideToggle();
 });
 // $(".view-rates-btn").click(function(){
 //   $(".icon-chevron-down").toggleClass("icon-chevron-up");
 // });
-$(".plus-minus-icons").click(function(){
+$(".plus-minus-icons").click(function () {
   $(".icon-plus-circle").toggleClass("icon-minus-circle");
 });
 
@@ -66,10 +66,10 @@ $('.hotel-gallery').on('click', function () {
 // Lazy Load
 document.addEventListener('DOMContentLoaded', function () {
   if (typeof LazyLoad != 'function') {
-      return;
+    return;
   }
   var lazyLoadInstance = new LazyLoad({
-      elements_selector: '.lazyload'
+    elements_selector: '.lazyload'
   });
 });
 
@@ -79,71 +79,72 @@ $('.loading-spinner').one('click', function () {
 });
 
 // Booking Widget
-  $('.promo-code-link').on('click', function (e) {
-    e.preventDefault();
-    $(this).next('.form-group').slideToggle();
-  });
-  
-  // Sliders
-  $('.standard-slider').slick({
-    arrows: true,
-    autoplay: true
-  });
+$('.promo-code-link').on('click', function (e) {
+  e.preventDefault();
+  $(this).next('.form-group').slideToggle();
+});
 
-  // Slick Initialize Modal
-  function initSlickSlider() {
-    setTimeout( function() {
-      // Slick slider single Item
-      $('.room-modal-slider').slick({
-        arrows: true,
-        autoplay: true
-      });
-  
-      // Modal Gallery
-      $('.gallery-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: true,
-        autoplay: false,
-        asNavFor: '.slider-nav'
-      });
-  
-      $('.slider-nav').slick({
-        // slidesToShow: 15,
-        slidesToScroll: 1,
-        infinite: false,
-        asNavFor: '.gallery-slider',
-        dots: false,
-        centerMode: true,
-        arrows: false,
-        vertical: false,
-        focusOnSelect: true
-      });
-    }, 500);
-  }
+// Sliders
+$('.standard-slider').slick({
+  arrows: false,
+  autoplay: true,
+  dots: true
+});
 
-  $('.btn').click(function(){
-    $('.modal-body').on('shown.bs.modal', function () {
-        initSlickSlider();
+// Slick Initialize Modal
+function initSlickSlider() {
+  setTimeout(function () {
+    // Slick slider single Item
+    $('.room-modal-slider').slick({
+      arrows: true,
+      autoplay: true
     });
-  });
-  $('.initSlick').click(function(){
+
+    // Modal Gallery
+    $('.gallery-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: true,
+      autoplay: false,
+      asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+      // slidesToShow: 15,
+      slidesToScroll: 1,
+      infinite: false,
+      asNavFor: '.gallery-slider',
+      dots: false,
+      centerMode: true,
+      arrows: false,
+      vertical: false,
+      focusOnSelect: true
+    });
+  }, 500);
+}
+
+$('.btn').click(function () {
+  $('.modal-body').on('shown.bs.modal', function () {
     initSlickSlider();
   });
-  $('.gallery-tab').click(function(){
-      initSlickSlider();
-  });
+});
+$('.initSlick').click(function () {
+  initSlickSlider();
+});
+$('.gallery-tab').click(function () {
+  initSlickSlider();
+});
 
-  $('.offers-form__filter-btn').click(function(){
-    $(this).closest('.offers-form').find('.offers-form__filter').slideToggle();
-  });
-  $('.offer-details__terms-link').click(function(){
-    $(this).closest('.offer-details__content').find('.offer-details__terms-content').slideToggle();
-  });
+$('.offers-form__filter-btn').click(function () {
+  $(this).closest('.offers-form').find('.offers-form__filter').slideToggle();
+});
+$('.offer-details__terms-link').click(function () {
+  $(this).closest('.offer-details__content').find('.offer-details__terms-content').slideToggle();
+});
 
 //Added 17/06/20
-$('.ibe-join-mobile-close').click(function(){
+$('.ibe-join-mobile-close').click(function () {
   $('.ibe-join-mobile').hide();
 });
 
